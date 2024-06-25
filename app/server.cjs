f08c734123b9d76a6e902063a39dacfd02bdd2c2
shqipron.popaj <shqipron.popaj@edu.fh-joanneum.at>
2024-06-25 20:25:15 +0200

    remove firebaseSDK.json

--- app/server.cjs
+++ app/server.cjs
@@ -5,7 +5,6 @@ const app = express();
 const PORT = 8080;
 const cookieParser = require('cookie-parser');
 const statusMonitor = require('express-status-monitor');
-const firebaseAdmin = require('firebase-admin');
 
 const port = process.env.PORT || 4000;
 
@@ -14,13 +13,6 @@ const {initializeApp, applicationDefault} = require('firebase-admin/app');
 app.use(cookieParser());
 app.use(express.json());
 
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseSDK.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://plan2travel4u-default-rtdb.europe-west1.firebasedatabase.app"
-});
 
 app.get("/", (req, res, next) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'html')});
@@ -43,28 +35,7 @@ app.use('/status', (req, res, next) => {
             const idToken = req.cookies.idToken;
             if (!idToken) {
                 return res.status(401).json({error: 'Unauthorized'});
-            }
-
-            firebaseAdmin.auth().verifyIdToken(idToken)
-                .then(decodedToken => {
-                    const database = firebaseAdmin.database();
-                    const ref = database.ref('users/' + decodedToken.uid + '/credentials');
-
-                    ref.once('value', snapshot => {
-                        const credential = snapshot.val();
-                        if (credential.payload.isAdmin) {
-                            req.user = decodedToken;
-                            next();
-                        } else {
-                            return res.status(401).json({error: 'Unauthorized'});
-                        }
-                    }).catch(err => {
-                        next(err)
-                    });
-                })
-                .catch(err => {
-                    next(err)
-                });
+            }            
         }
     }
 );
